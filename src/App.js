ca8fc0a269e944f291e4877ff8248b7d645576c0
subekshyakharel <147831342+subekshyakharel@users.noreply.github.com>
2024-06-11 12:27:20 +0545

    css added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const checkbox = [
+    {title : "Include Uppercase Letters", state : false}, 
+    { title : "Include Lowercase Letters", state: false}, 
+    {title: "Include Numbers", state : false}, 
+    {title: "Include symbols", state: false}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="wrapper">
+        <div className='Center'>
+        {/* password */}
+        <div className="pw">
+          <span>jksdn</span>
+          <button className='btn'>Copy</button>
+        </div>
+
+        {/* character choosen */}
+        <div className="character">
+          <div className='flex'>
+             <span>Character length</span>
+             <span id='length'>0</span>
+             </div>
+
+             <div>
+              <input type="range" 
+              min= "0"
+              max="20"
+              // value= "3" 
+              className='range'  
+              />
+             </div>
+
+              <div className="contain-checkbox">
+             {checkbox.map((item ,index)=>{
+              return (
+                <div className="checkbox">
+                <input type="checkbox" />
+                <label key={index}> {item.title}</label>
+               </div>
+              )
+             })}
+             </div>
+
+
+          {/* strength  */}
+          <div className="strength"></div>
+
+          {/* generator  */}
+          <button className='btn-generate'>Generate</button>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
